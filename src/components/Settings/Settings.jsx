0ce2a60d9966a22c3ac69eeaffce4c7be3231589
Stepawka <stepan-lvov-04@mail.ru>
2025-02-26 18:32:08 +0300

    feat: Заглушки пустых страниц

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,13 +1,20 @@
 import useTitle from '../../hooks/useTitle';
+import s from './Settings.module.css';
+
+import Loader from '../common/Loader/Loader';
+import withAuthRedirect from '../../utils/withAuthRedirect';
 
 const Settings = () => {
   useTitle('Settings');
 
   return (
-    <section>
-      Settings
+    <section className={s.settings}>
+      <h2 className={s.settings__title}>
+        Здесь пока нет никаких настроек...
+      </h2>
+      <Loader />
     </section>
   );
 }
 
-export default Settings;
+export default withAuthRedirect(Settings);
