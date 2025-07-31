430f6c816685fc5b8d0e343107a052d7a8098421
Stepawka <stepan-lvov-04@mail.ru>
2025-07-31 16:10:00 +0300

    styles: Добавить адаптивность Loader, Preloader

--- src/ui/loader/loader.tsx
+++ src/ui/loader/loader.tsx
@@ -1,13 +1,15 @@
-import { LoaderIcon } from "@icons";
-import clsx from "clsx";
 import { FC } from "react";
-import s from "./loader.module.css";
+import { LoaderIcon } from "@icons";
 import { LoaderProps } from "./types";
+import clsx from "clsx";
+import s from "./loader.module.css";
 
-export const Loader: FC<LoaderProps> = ({ loader, size = 96, classes }) => (
-  <div className={clsx(s.container, classes?.container)}>
-    {loader || (
-      <LoaderIcon size={size} className={clsx(s.loader, classes?.loader)} />
-    )}
-  </div>
-);
+export const Loader: FC<LoaderProps> = ({ loader, size = 80, classes }) => {
+  return (
+    <div className={clsx(s.container, classes?.container)}>
+      {loader || (
+        <LoaderIcon size={size} className={clsx(s.loader, classes?.loader)} />
+      )}
+    </div>
+  );
+};
