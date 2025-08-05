50bb932cb036be5a1bb88fbbae1fa1ff1280b873
Stepawka <stepan-lvov-04@mail.ru>
2025-08-05 20:23:56 +0300

    fix: disable submit when form is disabled

--- src/hooks/shared/useSubmitOnEnter.ts
+++ src/hooks/shared/useSubmitOnEnter.ts
@@ -1,18 +1,24 @@
 import { useCallback } from "react";
 
 type TUseSubmitOnEnterArgs = {
+  disabled: boolean;
   onSubmit: () => void;
 };
 
-export const useSubmitOnEnter = ({ onSubmit }: TUseSubmitOnEnterArgs) => {
+export const useSubmitOnEnter = ({
+  disabled,
+  onSubmit,
+}: TUseSubmitOnEnterArgs) => {
   const handleKeyDown = useCallback(
     (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
       if (event.key === "Enter" && !event.shiftKey) {
         event.preventDefault();
-        onSubmit();
+        if (!disabled) {
+          onSubmit();
+        }
       }
     },
-    [onSubmit]
+    [disabled, onSubmit]
   );
 
   return { handleKeyDown };
